c160535fd5b94cb41a0eb92d9b18d410651ec1c3
Nelson <uneatenbreakfast@gmail.com>
2019-12-13 00:49:11 +1300

    initial push

---
+++ Components/ToDoComponentController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTest.Components
+{
+    public class ToDoComponentController : ComponentBase
+    {
+        [Parameter] public string Tag { get; set; }
+
+        public IList<string> Items;
+        public string ItemInput = "Cats";
+
+        public ToDoComponentController()
+        {
+            Items = new List<string>{"one", "two","three"};
+        }
+
+        public IList<string> GetItems()
+        {
+            return Items;
+        }
+
+        public void UpdateList()
+        {
+            Items.Add(ItemInput);
+        }
+
+        public void UpdateList(string item)
+        {
+            Items.Add(item);
+        }
+    }
+}
